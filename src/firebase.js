c8e6cc5a5dc2f83161d37fb3a25f2da1c1ae2b75
jorgadev <radovanjorgic14@gmail.com>
2021-01-29 23:08:26 +0100

    Connected with database established

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import firebase from "firebase/app";
+import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-})
+  apiKey: "AIzaSyABcDZLhOScnJZpr-AVObIkYAb0fbe8Nhg",
+  authDomain: "code-teams.firebaseapp.com",
+  projectId: "code-teams",
+  storageBucket: "code-teams.appspot.com",
+  messagingSenderId: "759472991088",
+  appId: "1:759472991088:web:d282368045b86a0067fdb5",
+});
 
-export const auth = app.auth()
-export default app
+export const auth = app.auth();
+export default app;
